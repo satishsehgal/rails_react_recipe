898a321f463b010d4841cf1052f0ae77605a7a5d
satishsehgal <satish.k6677@gmail.com>
2024-01-01 01:47:56 +0530

    added recipe api controller and routesd

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'recipes/index'
+      get 'recipes/create'
+      get 'recipes/show'
+      get 'recipes/destroy'
+    end
+  end
   root 'homepage#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
