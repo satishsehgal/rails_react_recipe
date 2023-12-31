d0c9ab53c58a3bda09329d0368a818adf15f88e6
satishsehgal <satish.k6677@gmail.com>
2024-01-01 01:50:16 +0530

    added recipe create api

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -5,11 +5,23 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def create
+    recipe = Recipe.create!(recipe_params)
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def show
   end
 
   def destroy
+  end
+
+  private
+
+  def recipe_params
+    params.permit(:name, :image, :ingredients, :instruction)
   end
 end
